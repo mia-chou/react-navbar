54995bce1231410e13b324bb425f64fb4439ab9f
mia-chou <mia.fy.chou@hotmail.com>
2024-03-22 00:20:27 +0000

    Added CSS styling

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar(){
+return <nav className="nave">
+<a href="/" className="site-title" >Site Name</a>
+<ul>
+<li><a href="/pricing">Pricing</a></li>
+<li><a href="/about">About</a></li>
+
+</ul>
+</nav>
+
+
+}
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Import your main App component
+import reportWebVitals from './reportWebVitals'; // Import the reporting function
+import "./style.css";
+
+const root = document.getElementById('root'); // Find the root element in the HTML
+ReactDOM.createRoot(root).render( // Create a root and render your App component
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, call the reporting function
+// For example: reportWebVitals(console.log)
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
